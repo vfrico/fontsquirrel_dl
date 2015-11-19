d6bba3094d6eacf4018c6fbca73a3de96abc0a52
Víctor Fernández <vfrico@gmail.com>
2015-11-19 23:55:16 +0100

    adding search

--- test.py
+++ test.py
@@ -4,21 +4,27 @@
 #a.get_all_families("")
 
 from fontsquirrel import FontSquirrel
-a = FontSquirrel().get_font_list(force_download=True)
+from fuzzywuzzy import fuzz
+all_families = FontSquirrel().get_font_list(force_download=False)
 fuente = input("Busca esta fuente: ")
 
-found = False
-for i in a:
+found = []
+for i in all_families:
     name = i['family']
-    if name.lower() == fuente.lower():
-        found = True
-        fuente = i['family_url']
-        print("Estás buscando la fuente: "+name)
-        print (i)
-        break
-        
-if found:
-    print ("Descargando la fuente \""+fuente+"\" a ~/.fonts/ ")
-    FontSquirrel().get_family(fuente,"/home/vfrico/.fonts/pruebas/")
-else:
-    print("La fuente "+fuente+" no ha podido ser encontrada")
+
+    if fuzz.partial_ratio(fuente, name) > 70:
+        found.append(i['family_url'])
+
+#print(found)
+
+
+
+#else:
+#    print("La fuente "+fuente+" no ha podido ser encontrada")
+print("\nSe han encontrado {0} fuentes que coinciden con tu búsqueda:".format(len(found)))
+for opt in range(0,len(found)):
+    print('\t{0}:   {1}'.format(opt+1,found[opt]))
+
+option = input("Seleccione la opcion")
+print ("Descargando la fuente \""+found[option]+"\" a ~/.fonts/ ")
+FontSquirrel().get_family(found[option],"/home/victor/.fonts/pruebas/")
